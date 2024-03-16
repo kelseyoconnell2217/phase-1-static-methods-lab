06fbae71873f8a046d9da16898c676020b37f847
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-03-16 03:52:51 -0400

    Got it!

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9\-'" "]+/g, "")
+  }
+
+  static titleize(string){
+    let array = string.split(" ")
+    let newArray= []
+  const lowercaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+  for(let i of array){
+      if(!lowercaseWords.includes(i)){
+          newArray.push(Formatter.capitalize(i))
+        } else newArray.push(i)
+      }
+    let newString = Formatter.capitalize(newArray.join(" "))
+    return newString
+  } 
 }
+
+
+const titleizedString = Formatter.titleize("hello to the world");
+console.log(titleizedString);
